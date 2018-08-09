644d8784af1c474a3efc548ba02a2cf443c1007d
rileyp2000 <rileyp@bishopireton.org>
2018-08-09 09:55:50 -0400

    init

---
+++ Custom Compression/src/Compressor.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+public class Compressor {
+	private String uncompressed;
+	private String compressed;
+	private String key;
+	
+	public Compressor(String s){
+		uncompressed = s;
+		compressed = "";
+		key = "";
+	}
+	
+	public ArrayList<String> findPattern(int len){
+		ArrayList<String> ret = new ArrayList<String>();
+		for(int i = 0; i < uncompressed.length(); i ++){
+			
+		}
+	}
+}
---
+++ Custom Compression/src/Pattern.java
@@ -0,0 +1,21 @@
+
+public class Pattern {
+	private String s;
+	private int count;
+
+
+
+public Pattern(String ss){
+	s = ss;
+	count = 0;
+}
+
+public void incrementCount(){
+	count++;
+}
+
+public void reset(String ss){
+	count = 0;
+	s = ss;
+}
+}
